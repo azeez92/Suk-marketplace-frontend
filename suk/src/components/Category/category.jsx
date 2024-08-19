b3071a3d820721fde026596862c0b71e6fa248de
azeez92 <azeezsodiq18@gmail.com>
2024-08-19 13:18:12 +0100

    Delete suk directory

--- suk/src/components/Category/category.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import './category.css'
-
-const Category = () => {
-  <div>category section</div>
-}
-
-export default Category
-
--- suk/src/components/TrendingItem/trendingItem.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import './trending.css'
-
-const TrendingItem = () => {
-<div>trendingitem section</div>
-}
-
-export default TrendingItem
-
